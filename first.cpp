a8e9011131205cdf58514b6992230b5d8e03ccf7
hariomdoorwar <hariom.doorwar@kuliza.com>
2023-01-16 16:13:46 +0530

    commiting again

--- first.cpp
+++ first.cpp
@@ -17,12 +17,6 @@ int main()
         mp[s1[i]]++;
     }
 
-    // int i=0;
-   for(int i=0;i<s2.size();i++){
-    if(mp.find(s2[i])!=mp.end()){
-        ans=true;
-        break;
-    }
    }
 
     if(ans==true)cout<<"yes"<<endl;
